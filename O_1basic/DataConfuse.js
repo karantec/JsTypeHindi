5879bed44eeb74cde1dbe112da510a651d6225bd
KARAN RANA <103360966+karantec@users.noreply.github.com>
2024-02-16 19:12:41 +0000

    confuse

--- O_1basic/DataConfuse.js
+++ O_1basic/DataConfuse.js
@@ -1,22 +1,41 @@
-let score="hitesh";
-console.log(typeof score);
-console.log(typeof(score));
+// let score="hitesh";
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
-// "33"=>33
-//"33abc"=>Nan
-//true=>1;false=>0;
-let isLoggedIn="hitesh";
-let booleanisLoggedIn=Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);
-// 1=>true 
-// " "=>false
-// "hitesh"=>true
+// // "33"=>33
+// //"33abc"=>Nan
+// //true=>1;false=>0;
+// let isLoggedIn="hitesh";
+// let booleanisLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+// // 1=>true 
+// // " "=>false
+// // "hitesh"=>true
 
-let someNumber=33
-let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// **************operations ***********************
+let value=3
+let negValue=-value
+//console.log(negValue);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3);
+// console.log(2%3);
+// let str1="hello";
+// let str2="karan";
+// let str3=str1 +str2;
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1 +"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+console.log(true);

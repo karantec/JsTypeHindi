5694c3cda24a2811309bab0403809d4906766c32
KARAN RANA <103360966+karantec@users.noreply.github.com>
2024-02-16 18:29:47 +0000

    datatypes

---
+++ O_1basic/Datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version of code
+
+// alert(3+3) we are using node js , not browser
+
+console.log(3+3); //code readibility should be high
+console.log("hitest") 
+
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false;
+let state=null;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+//boolean=> true/false
+// null=>standard value
+//undefined=>
+//symbol=>unique
+//object 
+console.log(typeof undefined)//undefined 
+console.log(typeof null)//object

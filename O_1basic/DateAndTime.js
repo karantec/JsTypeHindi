4dd511374895b05413866da602b2660fa4bff0bf
KARAN RANA <103360966+karantec@users.noreply.github.com>
2024-02-16 20:58:17 +0000

    Date

---
+++ O_1basic/DateAndTime.js
@@ -0,0 +1,15 @@
+///Dates
+
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+// let myCreatedDate=new Date(2023,0,23);
+// let myCreatedDate=new Date(2023,0,23,5,23);
+// let myCreatedDate=new Date("2023-01-14");
+let myCreatedDate=new Date("01-14-2023");
+// console.log(myCreatedDate.toString());
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
+console.log(myTimeStamp);

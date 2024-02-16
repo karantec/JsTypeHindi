89fe99b2b43e53d0753b8b39e3e06776b4403c82
KARAN RANA <103360966+karantec@users.noreply.github.com>
2024-02-16 18:18:18 +0000

    variable

---
+++ O_1basic/variable.js
@@ -0,0 +1,18 @@
+const account_Id=14453
+let accountEmail="karanrana3095@gmail.com"
+var accountPassword="12345"
+
+accountCity="Jaipur"
+let accountState;
+accountEmail="sonutech04@gmail.com"
+accountPassword="121234523"
+accountCity="Delhi"
+
+// account_Id=2//not changeable
+
+
+console.log(account_Id);
+/*
+ Preferred not to use var because issues in block scope and 
+*/
+console.log([account_Id,accountEmail,accountPassword,accountCity])

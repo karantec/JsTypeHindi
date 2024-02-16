e2def8e035db2ed052b727e8bedf6615f3632e2a
KARAN RANA <103360966+karantec@users.noreply.github.com>
2024-02-16 20:25:56 +0000

    string

---
+++ O_1basic/String.js
@@ -0,0 +1,28 @@
+const name="karan"
+const repoCount=50;
+
+// console.log(name+repoCount + 
+//     "value" );
+// string interploation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName= new String("karan");
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf('t'));
+// const newString=gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString=gameName.slice(-5,2);
+// console.log(anotherString);
+const newStringnone="    karan   " 
+console.log(newStringnone.trim());
+const url="https://hitesh.com/hitesh%29hfkjhkhf";
+data=url.replace('hitesh','karan');
+console.log(data);
+console.log(url.includes("sundar"));
+console.log(split());

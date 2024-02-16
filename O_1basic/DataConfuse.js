fdbf096e61715de37494d597bbd526c4a284a8a2
KARAN RANA <103360966+karantec@users.noreply.github.com>
2024-02-16 18:56:13 +0000

    conversionofdatatype

---
+++ O_1basic/DataConfuse.js
@@ -0,0 +1,22 @@
+let score="hitesh";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33"=>33
+//"33abc"=>Nan
+//true=>1;false=>0;
+let isLoggedIn="hitesh";
+let booleanisLoggedIn=Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+// 1=>true 
+// " "=>false
+// "hitesh"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
